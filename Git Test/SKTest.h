6cbde4147a40bfedd144c38d9c3c86eb6d8389e9
Steve Kelley <steve@kelleymatthews.net>
2014-06-17 14:02:11 -0500

    added a test class

---
+++ Git Test/SKTest.h
@@ -0,0 +1,13 @@
+//
+//  SKTest.h
+//  Git Test
+//
+//  Created by Steve Kelley on 6/17/14.
+//  Copyright (c) 2014 Steve Kelley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKTest : NSObject
+
+@end
---
+++ Git Test/SKTest.m
@@ -0,0 +1,13 @@
+//
+//  SKTest.m
+//  Git Test
+//
+//  Created by Steve Kelley on 6/17/14.
+//  Copyright (c) 2014 Steve Kelley. All rights reserved.
+//
+
+#import "SKTest.h"
+
+@implementation SKTest
+
+@end
